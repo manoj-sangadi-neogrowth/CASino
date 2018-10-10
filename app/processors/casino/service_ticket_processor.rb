0460b88e365d0f2227d4236a7a96c88fbea729a9
Samrita Bakshi <samrita.bakshi@neogrowth.in>
2018-10-10 11:27:27 +0530

    ST for reuse

--- app/processors/casino/service_ticket_processor.rb
+++ app/processors/casino/service_ticket_processor.rb
@@ -69,7 +69,11 @@ module CASino::ServiceTicketProcessor
   def validate_existing_ticket_for_service(ticket, service, options = {})
     service = clean_service_url(service) if ticket.is_a?(CASino::ServiceTicket)
     if ticket.consumed?
-      ValidationResult.new 'INVALID_TICKET', "Ticket '#{ticket.ticket}' already consumed", :warn
+      if options[:is_api].present?
+        ValidationResult.new
+      else
+        ValidationResult.new 'INVALID_TICKET', "Ticket '#{ticket.ticket}' already consumed", :warn
+      end
     elsif ticket.expired?
       ValidationResult.new 'INVALID_TICKET', "Ticket '#{ticket.ticket}' has expired", :warn
     elsif service != ticket.service
