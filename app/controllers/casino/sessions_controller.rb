3ebe8897b3e9c41a52bf4b83f9e4d6d9f54506ec
Samrita Bakshi <samrita.bakshi@neogrowth.in>
2018-10-16 13:34:24 +0530

    gemfile

--- app/controllers/casino/sessions_controller.rb
+++ app/controllers/casino/sessions_controller.rb
@@ -48,14 +48,16 @@ class CASino::SessionsController < CASino::ApplicationController
 
 
   def logout
+    Rails.logger.info "in logout----params"
+    Rails.logger.info params
     ticket = params[:is_api].present? ? params[:ticket] : cookies[:tgt]
     sign_out(ticket)
     @url = params[:url]
     # if params[:service].present? && service_allowed?(params[:service])
     if params[:is_api]
       render json: { status: 'success', message: I18n.t('logout.logged_out_without_url') },status: :ok
-    elsif params[:destination].present? && service_allowed?(params[:destination])
-      redirect_to params[:destination], status: :see_other 
+    elsif params[:service].present? && service_allowed?(params[:service])
+      redirect_to params[:service], status: :see_other 
     else
       redirect_to login_path(service: params[:destination])
     end
