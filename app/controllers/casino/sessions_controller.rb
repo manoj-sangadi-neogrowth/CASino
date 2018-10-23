b1dd29959039703780d3d773f4128718a1fa0aee
Samrita Bakshi <samrita.bakshi@neogrowth.in>
2018-10-23 15:10:46 +0530

    login API chnage in response

--- app/controllers/casino/sessions_controller.rb
+++ app/controllers/casino/sessions_controller.rb
@@ -25,8 +25,6 @@ class CASino::SessionsController < CASino::ApplicationController
   end
 
   def create
-    p "params"
-    p params
     validation_result = validate_login_credentials(params[:username], params[:password])
     if !validation_result
       log_failed_login params[:username]
