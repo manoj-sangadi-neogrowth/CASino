9a183d74c42dc49f4eccf1e997b75abf91d7cd0d
Samrita Bakshi <samrita.bakshi@neogrowth.in>
2019-04-10 16:45:54 +0530

    user_agent

--- app/helpers/casino/sessions_helper.rb
+++ app/helpers/casino/sessions_helper.rb
@@ -74,7 +74,7 @@ module CASino::SessionsHelper
   def create_login_attempt(user, successful)
     user.login_attempts.create! successful: successful,
                                 user_ip: request.remote_ip,
-                                user_agent: user_agent
+                                user_agent: request.user_agent
   end
 
   private
