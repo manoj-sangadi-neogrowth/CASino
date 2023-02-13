6c5e3fddb0361fa323b952c9625b8353b47054f9
manoj-sangadi-neogrowth <bpo-manoj.sangadi@neogrowth.in>
2023-02-13 12:10:55 +0530

    small fix

---
+++ app/processors/casino/autenticators.rb
@@ -0,0 +1,51 @@
+require 'casino/authenticator'
+
+module CASino::Authenticators
+  extend ActiveSupport::Concern
+
+  def self.authenticators
+    @authenticators ||= {}.tap do |authenticators|
+      CASino.config[:authenticators].each do |name, auth|
+        next unless auth.is_a?(Hash)
+
+        authenticator = if auth[:class]
+                          auth[:class].constantize
+                        else
+                          load_authenticator(auth[:authenticator])
+                        end
+
+        authenticators[name] = authenticator.new(auth[:options])
+      end
+    end
+  end
+
+  private
+  def load_authenticator(name)
+    gemname, classname = parse_name(name)
+
+    begin
+      require gemname unless CASino.const_defined?(classname)
+      CASino.const_get(classname)
+    rescue LoadError => error
+      raise LoadError, load_error_message(name, gemname, error)
+    rescue NameError => error
+      raise NameError, name_error_message(name, error)
+    end
+  end
+
+  def parse_name(name)
+    [ "casino-#{name.underscore}_authenticator", "#{name.camelize}Authenticator" ]
+  end
+
+  def load_error_message(name, gemname, error)
+    "Failed to load authenticator '#{name}'. Maybe you have to include " \
+      "\"gem '#{gemname}'\" in your Gemfile?\n" \
+      "  Error: #{error.message}\n"
+  end
+
+  def name_error_message(name, error)
+    "Failed to load authenticator '#{name}'. The authenticator class must " \
+      "be defined in the CASino namespace.\n" \
+      "  Error: #{error.message}\n"
+  end
+end
